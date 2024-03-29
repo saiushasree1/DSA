0937cbe31117e86b4c8929490346ac08b393773a
saiushasree <122000744+saiushasree1@users.noreply.github.com>
2024-03-29 12:04:46 +0530

    29-03-2024

--- Binary Trees/PreOrderIterative.cpp
+++ Binary Trees/PreOrderIterative.cpp
@@ -1,16 +1,1 @@
-vector<int> preOrder(TreeNode<int> * root){
-    // Write your code here.
-    vector<int> ans;
-    if(!root) return ans;
-    stack<TreeNode*> s;
-    s.push(root);
-    while(!s.empty()){
-        ans.push_back(s.top()->data);
-        TreeNode* t=s.top();
-        s.pop();
-        if(t->right) s.push(t->right);
-        if(t->left) s.push(t->left);
 
-    }
-    return ans;
-}
