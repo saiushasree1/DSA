38626796d2a14610417205924b2b2e1cfd8e99e9
saiushasree <122000744+saiushasree1@users.noreply.github.com>
2024-04-10 00:58:07 +0530

    10-04-2024 Q2

---
+++ Dynamic Programming/MinimumNumebrOfDeletions.cpp
@@ -0,0 +1,26 @@
+int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.length(),m=text2.length();
+        int dp[n+1][m+1];
+        for(int i=0;i<n+1;i++){
+            dp[i][0]=0;
+        }
+        for(int i=0;i<m+1;i++){
+            dp[0][i]=0;
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    int minDistance(string word1, string word2) {
+        int len=longestCommonSubsequence(word1,word2);
+        int n=word1.length(),m=word2.length();
+        return n+m-2*len;
+    }
