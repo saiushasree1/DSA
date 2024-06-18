c6759b8dc558f9b60022ae90df6debff36494761
saiushasree <122000744+saiushasree1@users.noreply.github.com>
2024-06-18 18:57:59 +0530

    18-06-2024 Q5

---
+++ Heap/TopKFrequentElements.cpp
@@ -0,0 +1,20 @@
+vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q;
+        for(auto i:m){
+            q.push({i.second,i.first});
+            if(q.size()>k){
+                q.pop();
+            }
+        }
+        vector<int> ans;
+        while(!q.empty()){
+            ans.push_back(q.top().second);
+            q.pop();
+        }
+        return ans;
+    }
+https://leetcode.com/problems/top-k-frequent-elements/
