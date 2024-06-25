bd54d2f536322858f803e71e2a54df1b73c488d5
saiushasree <122000744+saiushasree1@users.noreply.github.com>
2024-06-25 14:28:13 +0530

    25-06-2024

--- Binary Search Trees/BstToGst.cpp
+++ Binary Search Trees/BstToGst.cpp
@@ -8,4 +8,5 @@ int rec(TreeNode* root,int t){
         int x=rec(root,0);
         return root;
     }
+
 https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/?envType=daily-question&envId=2024-06-25
