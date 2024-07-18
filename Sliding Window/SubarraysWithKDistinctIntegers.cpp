8d75eb3b28e8dfc5e836720123bcfebc7b4202bf
saiushasree <122000744+saiushasree1@users.noreply.github.com>
2024-07-18 18:32:30 +0530

    18-07-2024

---
+++ Sliding Window/SubarraysWithKDistinctIntegers.cpp
@@ -0,0 +1,25 @@
+int helpme(vector<int>&nums, int goal){
+        if(goal<0) return 0;
+        int n=nums.size();
+        int l=0,r=0;
+        int cnt=0;
+        unordered_map<int,int> mp;
+        while(r<n){
+            mp[nums[r]]++;
+            while(mp.size()>goal){
+                mp[nums[l]]--;
+                if(mp[nums[l]]==0){
+                    mp.erase(nums[l]);
+                }
+                l++;
+            }
+            cnt+=(r-l+1);
+            r++;
+        }
+        return cnt;
+    }
+
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return helpme(nums,k) - helpme(nums,k-1);
+    }
+https://leetcode.com/problems/subarrays-with-k-different-integers/
