b8a34952c2cda7977007c16f0834779926939e1f
saiushasree <122000744+saiushasree1@users.noreply.github.com>
2025-03-24 03:44:27 +0530

    Q3

---
+++ 800/1899.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(n%3==0) cout<<"Second"<<endl;
+        else cout<<"First"<<endl;
+    }
+    return 0;
+}
+https://codeforces.com/problemset/problem/1899/A
