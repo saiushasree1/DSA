e9def6d85d2738c4083a2fbaab3b65697a9384e0
saiushasree <122000744+saiushasree1@users.noreply.github.com>
2025-03-28 17:30:53 +0530

    Q11

---
+++ 800/1858A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+(c+1)/2 > b+ c/2) cout<<"First"<<endl;
+        else cout<<"Second"<<endl;
+    }
+    return 0;
+}
+https://codeforces.com/problemset/problem/1858/A
