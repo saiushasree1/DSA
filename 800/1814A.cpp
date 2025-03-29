d9389e1e1220aa95a207b0da1f28fb0b4d50eb38
saiushasree <122000744+saiushasree1@users.noreply.github.com>
2025-03-29 15:58:48 +0530

    Q21

---
+++ 800/1814A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+      long long n,k;
+      cin>>n>>k;
+      if(n%2==0 || (n-k>=0 && (n-k)%2==0)) cout<<"YES"<<endl;
+      else cout<<"NO"<<endl;
+    }
+    return 0;
+}
+https://codeforces.com/problemset/problem/1814/A
